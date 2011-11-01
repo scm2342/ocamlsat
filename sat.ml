bb249e633a292c2a25886451188d8f9c2703f1ae
sven mattsen <sven@sven.cc>
2011-11-01 19:27:50 +0100

    parser working

--- sat.ml
+++ sat.ml
@@ -1,11 +1,18 @@
-open Type
+open Type;;
+
+let rec print_prop a =
+    match a with
+        | Var a -> print_string a
+        | Not a -> print_string "~("; print_prop a; print_string ")"
+        | And(a, b) -> print_string "("; print_prop a; print_string " * "; print_prop b; print_string ")"
+        | Or(a, b) -> print_string "("; print_prop a; print_string " + "; print_prop b; print_string ")";;
 
 let _ =
-  try
-    let lexbuf = Lexing.from_channel stdin in
-    while true do
-      let result = Parser.main Lexer.token lexbuf in
-        print_newline(); flush stdout
-    done
+    try
+      let lexbuf = Lexing.from_channel stdin in
+      while true do
+          let result = Parser.main Lexer.token lexbuf in
+          print_prop result; print_newline(); flush stdout
+      done
   with Lexer.Eof ->
-    exit 0
+      exit 0;;
